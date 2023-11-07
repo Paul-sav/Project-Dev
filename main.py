b0fa98ab5892c1b7751fba3154098b66cc85a261
Paul-sav <111940575+Paul-sav@users.noreply.github.com>
2023-11-07 18:47:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 # Import configuration from config.py
 import config
-
+import machine
+import time
 # Your main application logic
 print("Running the main application...")
 
-#import machine
-#import time
-#led = machine.Pin(2, machine.Pin.OUT)
-#while True:
-#	led.value(1)
-#	time.sleep(1)
-#	led.value(0)
-#	time.sleep(1)
+led = machine.Pin(2, machine.Pin.OUT)
+while True:
+    led.value(1)
+    time.sleep(1)led.value(0)
+    time.sleep(1)
