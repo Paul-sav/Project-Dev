cb2318be9dfc8c6c0364120c2be3e6ecb5bb92d4
Paul-sav <111940575+Paul-sav@users.noreply.github.com>
2023-11-08 16:35:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ print("Running the main application...")
 afwd = machine.Pin(4, machine.Pin.OUT)
 arev = machine.Pin(15, machine.Pin.OUT)
 #while True:
-afwd.value(0)
-arev.value(1)
+afwd.value(1)
+arev.value(0)
 #    time.sleep(3)
     
 #    afwd.value(0)
