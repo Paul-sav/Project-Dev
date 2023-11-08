981bf800ccbc0c0ec26be8e2f1542eb899680b7e
Paul-sav <111940575+Paul-sav@users.noreply.github.com>
2023-11-08 16:20:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# Import configuration from config.py
 import config
 import machine
 import time
 # Your main application logic
 print("Running the main application...")
 
-led = machine.Pin(2, machine.Pin.OUT)
-led.value(1)
+afwd = machine.Pin(4, machine.Pin.OUT)
+arev = machine.Pin(15, machine.Pin.OUT)
+while True:
+    afwd.value(1)
+    arev.value(0)
+    time.sleep(3)
+    
+    afwd.value(0)
+    arev.value(1)
+    time.sleep(3)
+  
