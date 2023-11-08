6390e89c87642197cc6a5db98c6ace5d2f5ed7e8
Paul-sav <111940575+Paul-sav@users.noreply.github.com>
2023-11-07 19:06:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ import time
 print("Running the main application...")
 
 led = machine.Pin(2, machine.Pin.OUT)
-while True:
-    led.value(1)
-    time.sleep(1)
-    led.value(0)
-    time.sleep(1)
+led.value(1)
