628265673ba1aa5ec3f09373644d754c9aa5b1e7
Paul-sav <111940575+Paul-sav@users.noreply.github.com>
2023-11-08 17:00:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import config
 import machine
 import time
+# Initialize PWM objects for pins 4 and 15
+pwm_pin4 = machine.PWM(machine.Pin(4))
+pwm_pin15 = machine.PWM(machine.Pin(15))
+
+# Set the PWM frequency to 50 kHz (50000 Hz)
+pwm_pin4.freq(50000)
+pwm_pin15.freq(50000)
+
 # Your main application logic
 print("Running the main application...")
 
-afwd = machine.Pin(4, machine.Pin.OUT)
-arev = machine.Pin(15, machine.Pin.OUT)
-#while True:
-afwd.value(0)
-arev.value(0)
-#    time.sleep(3)
-    
-#    afwd.value(0)
-#    arev.value(1)
-#    time.sleep(3)
-  
+while True:
+	# Alternate between pins 4 and 15
+	pwm_pin4.duty(0)
+	pwm_pin15.duty(512)  # 50% duty cycle
+	time.sleep(2)  # Wait for 2 seconds
+	
+	pwm_pin4.duty(512)  # 50% duty cycle
+	pwm_pin15.duty(0)
+	time.sleep(2)  # Wait for 2 seconds
