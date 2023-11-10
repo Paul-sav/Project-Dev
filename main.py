63a14ca7210af31c418752209ac3c71dd0d0ba71
Paul-sav <paulsavard5@gmail.com>
2023-11-09 21:39:20 -0500

    twat4

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ wheel_stby = machine.Pin(33, machine.Pin.OUT); wheel_stby.value(1)
 wheel_PWM_B.duty(600)
 wheel_PWM_A.duty(1000)
 wheel_B1.value(0)
-wheel_B2.value(1)
+wheel_B2.value(0)
 wheel_A1.value(0)
 wheel_A2.value(1)
 while 1:
