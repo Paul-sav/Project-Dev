7e17eaaf26c3ff38f39abaa9fc274c31f2a49424
Paul Savard <paulsavard5@gmail.com>
2023-11-17 13:55:01 -0500

    blank push

--- main.py
+++ main.py
@@ -3,26 +3,25 @@ import config
 import machine
 import time
 
+# region Pin settings for directions
+# Actuator Down
+# afwd.value(0)
+# arev.value(1)
 
+# Actuator Up
+# afwd.value(1)
+# arev.value(0)
 
-#while 1:
-    # afwd.value(0)
-    # arev.value(1)
-    # time.sleep(1)
-    # afwd.value(1)
-    # arev.value(0)
-    # time.sleep(1)
-    # Forward
-    # wheel_B1.value(1)
-    # wheel_B2.value(0)
-    # wheel_A1.value(0)
-    # wheel_A2.value(1)
-    # time.sleep(5)
-    # Backward
-    # wheel_B1.value(0)
-    # wheel_B2.value(1)
-    # wheel_A1.value(1)
-    # wheel_A2.value(0)
-    # time.sleep(5)
+# Wheel Forward
+# wheel_B1.value(1)
+# wheel_B2.value(0)
+# wheel_A1.value(0)
+# wheel_A2.value(1)
 
+# Wheel Reverse
+# wheel_B1.value(0)
+# wheel_B2.value(1)
+# wheel_A1.value(1)
+# wheel_A2.value(0)
+# endregion
 
