fd5ef8ff5cacf9e5b93080d95bf55c733d932318
Paul Savard <paulsavard5@gmail.com>
2023-11-17 12:51:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ wheel_B2.value(1)
 
 wheel_A1.value(0)
 wheel_A2.value(1)
-while 1:
-    afwd.value(0)
-    arev.value(1)
-    time.sleep(1)
-    afwd.value(1)
-    arev.value(0)
-    time.sleep(1)
+#while 1:
+    # afwd.value(0)
+    # arev.value(1)
+    # time.sleep(1)
+    # afwd.value(1)
+    # arev.value(0)
+    # time.sleep(1)
     # Forward
     # wheel_B1.value(1)
     # wheel_B2.value(0)
