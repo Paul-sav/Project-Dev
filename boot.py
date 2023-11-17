2757cacd6150014c983caff9ee18d652a12785ef
Paul Savard <paulsavard5@gmail.com>
2023-11-17 12:55:31 -0500

    boot

--- boot.py
+++ boot.py
@@ -9,6 +9,28 @@ import urequests
 import time
 import config  # Ensure that you have a config.py file with WIFI_SSID and WIFI_PASS
 
+# region Pin initialization
+# Actuator initialization
+afwd = machine.Pin(4, machine.Pin.OUT); afwd.value(0)
+arev = machine.Pin(15, machine.Pin.OUT); arev.value(0)
+
+# PWM for motor wheels
+wheel_PWM_B = machine.PWM(machine.Pin(25))
+wheel_PWM_B.freq(20000)
+wheel_PWM_B.duty(0)
+
+wheel_PWM_A = machine.PWM(machine.Pin(26))
+wheel_PWM_A.freq(20000)
+wheel_PWM_A.duty(0)
+
+# Wheel initialization
+wheel_B1 = machine.Pin(12, machine.Pin.OUT); wheel_B1.value(0)
+wheel_B2 = machine.Pin(27, machine.Pin.OUT); wheel_B2.value(0)
+wheel_A1 = machine.Pin(13, machine.Pin.OUT); wheel_A1.value(0)
+wheel_A2 = machine.Pin(14, machine.Pin.OUT); wheel_A2.value(0)
+wheel_stby = machine.Pin(33, machine.Pin.OUT); wheel_stby.value(1)
+# endregion
+
 # region Github and Wi-Fi info
 # GitHub repository information
 GITHUB_USER = "Paul-sav"
@@ -22,8 +44,8 @@ sta_if.connect(config.WIFI_SSID, config.WIFI_PASS)
 # endregion
 
 # region Connecting to Wi-Fi and parsing GitHub repo
-# Wait for Wi-Fi connection (15 sec timeout)
-timeout = 15
+# Wait for Wi-Fi connection (5 sec timeout)
+timeout = 5
 while not sta_if.isconnected() and timeout > 0:
     time.sleep(1)
     timeout -= 1
