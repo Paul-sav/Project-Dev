4a05b329e0d0ed41cbb0f8b9aae4d6fc6662c7c7
Paul-sav <paulsavard5@gmail.com>
2023-11-21 10:42:23 -0500

    udp test

---
+++ UDPClient.py
@@ -0,0 +1,47 @@
+import socket
+
+# Broadcast discovery message to find ESP32
+BROADCAST_PORT = 8080
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client_socket.sendto('ESP32 Discovery'.encode(), ('<broadcast>', BROADCAST_PORT))
+
+# Listen for replies
+client_socket.settimeout(5)  # Set a timeout to wait for replies
+try:
+    while True:
+        data, addr = client_socket.recvfrom(1024)
+        esp_ip = data.decode()
+        print(f"ESP found at: {esp_ip}")
+        break  # Once a reply is received, stop listening
+except socket.timeout:
+    print("Discovery complete")
+finally:
+    client_socket.close()
+
+# Send commands to the discovered ESP32
+if 'esp_ip' in locals():  # Check if the ESP32 was found
+    ESP_IP = esp_ip  # Use the discovered ESP32's IP address
+    ESP_PORT = 8080  # Replace with the UDP port your ESP32 is listening on
+
+    # Create a UDP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Function to send commands to ESP32
+    def send_command(command):
+        client_socket.sendto(command.encode(), (ESP_IP, ESP_PORT))
+        print(f"Sent: {command}")
+
+
+    # Send commands after Enter is pressed
+    while True:
+        user_input = input("Enter a command: ")
+        if user_input.lower() == 'exit':
+            break  # Exit loop on 'exit' command
+        send_command(user_input)
+
+    # Close the socket when done
+    client_socket.close()
+else:
+    print("ESP not found")
--- main.py
+++ main.py
@@ -1,59 +1,130 @@
 import config
 import machine
 import time
+import socket
 
-wheel_PWM_B.duty(700)
-wheel_PWM_A.duty(650)
 
-while True:
+def setup_udp_server(port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('0.0.0.0', port))  # Bind to all available interfaces
+    print(f"UDP server started on port {port}")
+
+
+def move_forward():
     wheel_B1.value(1)
     wheel_B2.value(0)
     wheel_A1.value(0)
     wheel_A2.value(1)
-    time.sleep(1)
 
-    wheel_B1.value(1)
-    wheel_B2.value(0)
+
+def move_backward():
+    wheel_B1.value(0)
+    wheel_B2.value(1)
     wheel_A1.value(1)
     wheel_A2.value(0)
-    time.sleep(1)
 
+
+def turn_left():
     wheel_B1.value(0)
     wheel_B2.value(1)
     wheel_A1.value(0)
     wheel_A2.value(1)
+
+
+def turn_right():
+    wheel_B1.value(1)
+    wheel_B2.value(0)
+    wheel_A1.value(1)
+    wheel_A2.value(0)
+
+
+def stop_robot():  # Sets all outputs to LOW
+    wheel_B1.value(0)
+    wheel_B2.value(0)
+    wheel_A1.value(0)
+    wheel_A2.value(0)
+    afwd.value(0)
+    arev.value(0)
+
+
+def actuator_up():  # Raises actuator for 1s
+    afwd.value(0)
+    arev.value(1)
     time.sleep(1)
+    afwd.value(0)
+    arev.value(0)
 
-# region Pin settings for directions
-# Actuator Down
-# afwd.value(0)
-# arev.value(1)
 
-# Actuator Up
-# afwd.value(1)
-# arev.value(0)
+def actuator_down():  # Lowers actuator for 1s
+    afwd.value(1)
+    arev.value(0)
+    time.sleep(1)
+    afwd.value(0)
+    arev.value(0)
 
-# Wheel Forward
-# wheel_B1.value(1)
-# wheel_B2.value(0)
-# wheel_A1.value(0)
-# wheel_A2.value(1)
 
-# Wheel Reverse
-# wheel_B1.value(0)
-# wheel_B2.value(1)
-# wheel_A1.value(1)
-# wheel_A2.value(0)
+# Map commands to functions
+command_functions = {
+    'w': move_forward,
+    's': move_backward,
+    'a': turn_left,
+    'd': turn_right,
+    'x': stop_robot,
+    'q': actuator_up,
+    'e': actuator_down
+}
 
-# Turn Left
-# wheel_B1.value(0)
-# wheel_B2.value(1)
-# wheel_A1.value(0)
-# wheel_A2.value(1)
-#
-# Turn Right
-# wheel_B1.value(1)
-# wheel_B2.value(0)
-# wheel_A1.value(1)
-# wheel_A2.value(0)
+# region Pin initialization
+# Actuator initialization
+afwd = machine.Pin(4, machine.Pin.OUT)
+afwd.value(0)
+arev = machine.Pin(15, machine.Pin.OUT)
+arev.value(0)
+
+# PWM for motor wheels
+wheel_PWM_B = machine.PWM(machine.Pin(25))
+wheel_PWM_B.freq(20000)
+wheel_PWM_B.duty(700)
+
+wheel_PWM_A = machine.PWM(machine.Pin(26))
+wheel_PWM_A.freq(20000)
+wheel_PWM_A.duty(650)
+
+# Wheel initialization
+wheel_B1 = machine.Pin(12, machine.Pin.OUT)
+wheel_B1.value(0)
+wheel_B2 = machine.Pin(27, machine.Pin.OUT)
+wheel_B2.value(0)
+wheel_A1 = machine.Pin(13, machine.Pin.OUT)
+wheel_A1.value(0)
+wheel_A2 = machine.Pin(14, machine.Pin.OUT)
+wheel_A2.value(0)
+wheel_stby = machine.Pin(33, machine.Pin.OUT)
+wheel_stby.value(1)
 # endregion
+
+UDP_PORT = 8080
+setup_udp_server(UDP_PORT)
+
+while True:
+    data, addr = server_socket.recvfrom(1024)
+    print("Received:", data.decode('utf-8'), "from", addr)
+
+    if message.strip() == 'exit':
+        break
+
+    command = message.strip().lower()
+    command_function = command_functions.get(command)
+    if command_function:
+        command_function()
+    else:
+        print("Unknown command")
+
+    # Respond with the ESP32's IP address upon receiving a broadcast message
+    if message.strip().lower() == 'esp32 discovery':
+        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp.sendto(str(addr[0]).encode(), ('<broadcast>', port))
+
+server_socket.close()
+print("UDP server stopped")
